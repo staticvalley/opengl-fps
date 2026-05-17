3389dfac5045e15b69651bd81656b3fabb91e57d
Jonathan Kocevar <jkocevar@uoguelph.ca>
2026-05-17 05:29:10 -0400

    [feat](collision): added createFromPoint option for camera

--- include/physics/Collision.hpp
+++ include/physics/Collision.hpp
@@ -6,5 +6,9 @@ struct AABB {
 	glm::vec3 min;
 	glm::vec3 max;
 
+	// check if box intersects with other box
 	bool intersects(const AABB& otherBox);
+
+	// create bounding box given position and box size
+	static AABB createFromPoint(const glm::vec3& position, const glm::vec3& boxSize);
 };
--- src/physics/Collision.cpp
+++ src/physics/Collision.cpp
@@ -1,4 +1,4 @@
-#include <Collision.h>
+#include <Collision.hpp>
 
 bool AABB::intersects(const AABB& otherBox) {
 	/* for each axis:
@@ -8,4 +8,9 @@ bool AABB::intersects(const AABB& otherBox) {
 	return	min.x <= otherBox.max.x && max.x >= otherBox.min.x &&
 			min.y <= otherBox.max.y && max.y >= otherBox.min.y &&
 			min.z <= otherBox.max.z && max.z >= otherBox.min.z;
+}
+
+AABB AABB::createFromPoint(const glm::vec3& position, const glm::vec3& boxSize) {
+	glm::vec3 half = boxSize * 0.5f;
+	return { position - half, position + half };
 }
