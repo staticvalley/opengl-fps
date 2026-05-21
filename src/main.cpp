df3e3b015b2e2c1941badb88b2ea954dcaa98a81
Jonathan Kocevar <jkocevar@uoguelph.ca>
2026-05-21 05:13:19 -0400

    [feat](project): working bsp map loaded with basic player clipnode collision

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 
 #include <glad/glad.h>
 
+#include <print>
+
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_opengl.h>
 #include <SDL3/SDL_main.h>
@@ -13,29 +15,37 @@
 #include "imgui_impl_sdl3.h"
 #include "imgui_impl_opengl3.h"
 
+#include <Player.hpp>
 #include <Shader.hpp>
 #include <Camera.hpp>
 #include <Mesh.hpp>
 #include <Model.hpp>
 #include <Skybox.hpp>
 #include <Lighting.hpp>
+#include <Collision.hpp>
+#include <BSPMap.hpp>
+
+#include <DebugDrawer.hpp>
+#include <DebugUtils.hpp>
 
 #define WINDOW_WIDTH 800.0f
 #define WINDOW_HEIGHT 600.0f
 
 SDL_Window* window = nullptr;
 SDL_GLContext opengl_context = nullptr;
-Camera* camera;
 Shader* basic;
-Model* gman;
-Model* silenthill;
 Skybox* skybox;
-Shader* skyboxShader;
+Player* player;
 PointLight pl;
-Model* unitsquare;
 GLfloat lastTime = 0.0f;
+BSPMap* map;
+DirectionalLight sun;
 
-bool hasFlashlightOn = true;
+bool debugHudOn = false;
+bool lightOn = true;
+
+void drawDebugHud();
+FPSCounter fps;
 
 // program initialization
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv) {
@@ -88,14 +98,22 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv) {
     // hide mouse when window is focused
     SDL_SetWindowRelativeMouseMode(window, true);
 
+#ifdef DEBUG
     // create imgui context and bind to sdl3 window
     ImGui::CreateContext();
-    ImGui::StyleColorsLight();
+    ImGui::StyleColorsDark();
+
+    ImGuiStyle& style = ImGui::GetStyle();
+    float main_scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
+    style.ScaleAllSizes(main_scale);        // bake a fixed style scale
+    style.FontScaleDpi = main_scale;        // set initial font scale
 
     ImGui_ImplSDL3_InitForOpenGL(window, opengl_context);
     ImGui_ImplOpenGL3_Init("#version 460");
 
+    DebugDrawer::init();
 
+#endif
 
     const char* skyboxFilePaths[] = {
         "assets/textures/skybox/left.jpg",
@@ -108,27 +126,31 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv) {
 
     skybox = new Skybox(skyboxFilePaths, new Shader("assets/shaders/skybox.vert", "assets/shaders/skybox.frag"));
 
-    gman = Model::loadOBJ("assets/models/gman.obj");
-    gman->position = glm::vec3(-1.0f, 0.0f, 0.0f);
-    gman->scale = glm::vec3(2.0f);
-    gman->setTexture(new Texture("assets/textures/gman.png"));
+    map = new BSPMap("assets/game/de_inferno.bsp");
 
-    silenthill = Model::loadOBJ("assets/models/heathermason.obj");
-    silenthill->position = glm::vec3(1.0f, 0.0f, 0.0f);
-    silenthill->scale = glm::vec3(0.003f);
-    silenthill->setTexture(new Texture("assets/textures/heathermason.png"));
-
-    unitsquare = Model::loadOBJ("assets/models/unitsquare.obj");
-    unitsquare->position = glm::vec3(1.1f, 0.0f, -2.0f);
-    unitsquare->setTexture(new Texture("assets/textures/flat.jpg"));
+    map->nullTexture = new Texture("assets/textures/missing_content.png");
 
     basic = new Shader("assets/shaders/phong.vert", "assets/shaders/phong.frag");
 
-    camera = new Camera(
-        glm::vec3(1.0, 1.0, 2.0),
-        90.0f,
-        WINDOW_WIDTH / WINDOW_HEIGHT
-    );
+    player = nullptr;
+    for (const BSPEntity& entity : map->bsp.entities()) {
+        if (entity.at("classname") == "info_player_start") {
+            std::string origin = entity.at("origin");
+            float x, y, z;
+            sscanf(origin.c_str(), "%f %f %f", &x, &y, &z);
+            glm::vec3 spawnPos = glm::vec3(x, z*2, -y) * 0.025f;
+            SDL_Log("spawn point found: %.2f %.2f %.2f", spawnPos.x, spawnPos.y, spawnPos.z);
+            player = new Player(spawnPos, 90.0f, WINDOW_WIDTH / WINDOW_HEIGHT);
+            break;
+        }
+    }
+    // fallback if no spawn found
+    if (!player) {
+        SDL_Log("no spawn point found, using default");
+        player = new Player(glm::vec3(0, 0, 0), 90.0f, WINDOW_WIDTH / WINDOW_HEIGHT);
+    }
+
+    sun.ambient_color = glm::vec3(1.0f, 1.0f, 1.0f);
 
     pl.position = glm::vec3(0, 0, 0);
     pl.ambient_color = glm::vec3(0.2);
@@ -145,31 +167,28 @@ SDL_AppResult SDL_AppIterate(void* appstate) {
     float deltaTime = currentTime - lastTime;
     lastTime = currentTime;
 
-    camera->processKeyboard(deltaTime);
+    player->update(deltaTime, *map);
 
     // clear color and depth buffers
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glClearColor(0.1f, 0.1f, 0.15f, 1.0f);
 
-    //gman->rotation.y = gman->rotation.y += 1.0f;
-    //silenthill->rotation.y = silenthill->rotation.y += 1.0f;
-
     basic->bind();
 
     // camera uniforms
-    basic->setUniformMat4("u_view", camera->getViewMatrix());
-    basic->setUniformMat4("u_projection", camera->getProjectionMatrix());
-    basic->setUniformVec3("u_camera_position", camera->position);
+    basic->setUniformMat4("u_view", player->camera->getViewMatrix());
+    basic->setUniformMat4("u_projection", player->camera->getProjectionMatrix());
+    basic->setUniformVec3("u_camera_position", player->camera->getPosition());
 
     // light uniforms
     basic->setUniformInt1("u_spot_light_count", 0);
-    basic->setUniformBool1("u_has_directional_light", false);
-    
+    basic->setUniformBool1("u_has_directional_light", true);
+
+    sun.apply(*basic);
+
     pl.position = glm::vec3(0, sin(currentTime) + 0.5, -0.5);
 
-    //pl.position = glm::vec3(sin(currentTime)+0.5, 0, cos(currentTime) + 0.5);
-
-    if (hasFlashlightOn) {
+    if (lightOn) {
         basic->setUniformInt1("u_point_light_count", 1);
         pl.apply(*basic, 0);
     }
@@ -177,27 +196,14 @@ SDL_AppResult SDL_AppIterate(void* appstate) {
         basic->setUniformInt1("u_point_light_count", 0);
     }
 
-    gman->draw(*basic);
-    silenthill->draw(*basic);
-    unitsquare->draw(*basic);
+    map->draw(*basic);
 
-    //skybox->draw(*camera);
-    
-#ifdef DEBUG
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplSDL3_NewFrame();
+    skybox->draw(*(player->camera));
 
-    ImGui::NewFrame();
-
-    ImGui::Begin("Debug");
-    ImGui::Text("camera pos [%.2lf %.2lf %.2lf]", camera->position.x, camera->position.y, camera->position.z);
-    ImGui::Text("camera view [%.2lf %.2lf %.2lf]", camera->forward.x, camera->forward.y, camera->forward.z);
-    ImGui::End();
-
-    ImGui::Render();
-
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-#endif
+    if (debugHudOn) {
+        fps.tick();
+        drawDebugHud();
+    }
 
     // switch buffer to screen
     SDL_GL_SwapWindow(window);
@@ -209,15 +215,23 @@ SDL_AppResult SDL_AppIterate(void* appstate) {
 SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
     switch (event->type) {
     case SDL_EVENT_MOUSE_MOTION:
-        camera->processMouse(event->motion.xrel, event->motion.yrel);
+        player->processMouse(event->motion.xrel, event->motion.yrel);
         break;
     case SDL_EVENT_WINDOW_RESIZED:
         // set new aspect ratio for projection
-        camera->aspectRatio = (float)event->window.data1 / (float)event->window.data2;
+        player->camera->updateAspectRatio((float)event->window.data1, (float) event->window.data2);
         glViewport(0, 0, event->window.data1, event->window.data2);
         break;
     case SDL_EVENT_KEY_DOWN:
-        if (event->key.scancode == SDL_SCANCODE_F) hasFlashlightOn = !hasFlashlightOn;
+        if (event->key.scancode == SDL_SCANCODE_F) lightOn = !lightOn;
+        if (event->key.scancode == SDL_SCANCODE_F3) debugHudOn = !debugHudOn;
+        if (event->key.scancode == SDL_SCANCODE_N) {
+            if (player->movement.moveType == MovementType::WALK) {
+                player->movement.moveType = MovementType::NOCLIP;
+            } else {
+                player->movement.moveType = MovementType::WALK;
+            }
+        }
         break;
     case SDL_EVENT_QUIT:
         return SDL_APP_SUCCESS;
@@ -230,12 +244,52 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
 
 // no cleanup needed, sdl cleans up its own entities
 void SDL_AppQuit(void* appstate, SDL_AppResult result) {
-    delete camera;
+    delete player;
     delete basic;
-    delete gman;
-    delete silenthill;
     delete skybox;
+#ifdef DEBUG
+    DebugDrawer::cleanup();
+#endif 
     SDL_GL_DestroyContext(opengl_context);
     SDL_DestroyWindow(window);
     SDL_Quit();
+}
+
+void drawDebugHud() {
+
+    glm::vec3 cameraPosition = player->camera->getPosition();
+    glm::vec3 cameraForward = player->camera->getForwardDirection();
+
+    glm::vec3 velocity = player->movement.velocity;
+    glm::vec3 origin = player->movement.position;
+
+    DebugDrawer::draw3DGizmo(*(player->camera));
+    //for (SceneObject& obj : sceneObjectList)
+    //    DebugDrawer::drawAABB(*(player->camera), obj.model->boundingBox);
+    DebugDrawer::drawAABB(
+        *(player->camera),
+        AABB::createFromPoint(player->position() + glm::vec3(0.0f, 1.6f, 0.0f), glm::vec3(0.5f, 1.8, 0.5f)),
+        glm::vec3(1.0f, 0.0f, 0.0f)
+    );
+
+    DebugDrawer::drawPoint(*(player->camera), origin, glm::vec3(1, 0, 0), 0.2);
+    DebugDrawer::drawPoint(*(player->camera), glm::vec3(63,2,2), glm::vec3(0, 1, 1), 0.2);
+
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplSDL3_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::SetNextWindowSize(ImVec2(0, 0));
+
+    ImGui::Begin("Debug");
+    ImGui::Text("camera pos [%.2lf %.2lf %.2lf]", cameraPosition.x, cameraPosition.y, cameraPosition.z);
+    ImGui::Text("camera view [%.2lf %.2lf %.2lf]", cameraForward.x, cameraForward.y, cameraForward.z);
+    ImGui::Text("origin [%.2lf %.2lf %.2lf]", origin.x, origin.y, origin.z);
+    ImGui::Text("velocity [%.2lf %.2lf %.2lf]", velocity.x, velocity.y, velocity.z);
+    ImGui::Text(fps.fpsString.c_str());
+    ImGui::End();
+
+    ImGui::Render();
+
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
